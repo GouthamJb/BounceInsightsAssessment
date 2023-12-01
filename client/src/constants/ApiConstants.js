a1e6aa4d859926bb37e76776aea572b88a352365
gouthamj <gouthamj@qburst.com>
2023-12-01 06:34:56 +0000

    vercel test config

--- client/src/constants/ApiConstants.js
+++ client/src/constants/ApiConstants.js
@@ -1,11 +1,11 @@
 let BASE_URL;
 
-if (process.env.STAGE == 'production') {
+if (process.env.REACT_APP_STAGE == 'production') {
   // Deployed URL
   BASE_URL = "https://bounce-insights-assessment.vercel.app/api/country";
 } else {
   // Localhost URL
-  
+
   BASE_URL = "http://localhost:8000/api/country";
 }
 
--- client/src/reducers/CountryReducer.js
+++ client/src/reducers/CountryReducer.js
@@ -10,7 +10,7 @@ export const countryDetails = createAsyncThunk(
   "country",
   async (country, { rejectWithValue }) => {
     try {
-      console.log(process.env.STAGE)
+      console.log(process.env.REACT_APP_STAGE)
       const response = await fetch(`${BASE_URL}?name=${country}`, {
         headers: {
           "Access-Control-Allow-Origin": "*",
