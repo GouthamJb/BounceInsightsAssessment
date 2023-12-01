fe1b7e9365cbb478574c935c85b672b96d4310e6
gouthamj <gouthamj@qburst.com>
2023-12-01 06:10:07 +0000

    config modified for vercel

--- client/src/constants/ApiConstants.js
+++ client/src/constants/ApiConstants.js
@@ -1,1 +1,11 @@
-export const BASE_URL = "http://localhost:8000/api/country";
+let BASE_URL;
+
+if (process.env.VERCEL_ENV === 'production') {
+  // Deployed URL
+  BASE_URL = "https://bounce-insights-assessment.vercel.app/api/country";
+} else {
+  // Localhost URL
+  BASE_URL = "http://localhost:8000/api/country";
+}
+
+export { BASE_URL };
